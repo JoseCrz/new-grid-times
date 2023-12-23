56419120210e10e3e595cb97ce9c76087652677b
Jose Cuevas <josecueram@gmail.com>
2023-12-23 10:00:06 -0600

    Solve exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
     <header>
+      <MobileHeader />
+      <DesktopHeader />
+    </header>
+  );
+};
+
+function DesktopHeader() {
+  return (
+    <DesktopHeaderWrapper>
+      <ActionGroup>
+        <button>
+          <Search size={24} />
+        </button>
+        <button>
+          <Menu size={24} />
+        </button>
+      </ActionGroup>
+      <Logo />
+      <div>
+        <Button>Subscribe</Button>
+        <a
+          href="localhost:3000#"
+          style={{
+            display: "block",
+            textDecoration: "underline",
+            marginTop: "8px",
+            fontSize: "1rem",
+          }}
+        >
+          Already a subscriber?
+        </a>
+      </div>
+    </DesktopHeaderWrapper>
+  );
+}
+
+function MobileHeader() {
+  return (
+    <MobileHeaderWrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -28,17 +67,36 @@ const Header = () => {
           </ActionGroup>
         </Row>
       </SuperHeader>
-      <MainHeader>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "32px",
+          marginBottom: "48px",
+        }}
+      >
         <Logo />
-      </MainHeader>
-    </header>
+      </div>
+    </MobileHeaderWrapper>
   );
-};
+}
+
+const MobileHeaderWrapper = styled.div`
+  display: block;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +117,15 @@ const ActionGroup = styled.div`
   }
 `;
 
-const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
+const DesktopHeaderWrapper = styled(MaxWidthWrapper)`
+  display: none;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+  }
 `;
 
 export default Header;
